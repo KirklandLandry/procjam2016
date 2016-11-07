40fea636e7a01200e82028e4d9d92d23b24ac646
kirk <sonare2@hotmail.com>
2016-11-06 21:02:59 -0500

    thinking about how to add a map

--- procgenTyping/game.lua
+++ procgenTyping/game.lua
@@ -1,15 +1,66 @@
 
 -- parallel universe typing simulator 2016
 
-keys = {}
-
- 
- local keyMap = {}
+local keys = {}
+local keyMap = {}
  
 local topRow = "qwertyuiop"
 local secondRow = "asdfghjkl"
 local thirdRow = "zxcvbnm"
  
+
+local camera = {
+	x = 0, 
+	y = 0,
+	vx = 0,
+	vy = 0,
+	acceleration = 100,
+	friction = 0.1,
+	scale = 0.5,
+	rotation = 0
+}
+
+
+local tileWidth = 30 
+local tileHeight = 30
+
+
+
+local map = {}
+
+
+local roomLayout = {
+	{1, 1, 0, 1, 1},
+	{1, 0, 0, 0, 1}, 
+	{0, 0, 0, 0, 0},
+	{1, 0, 0, 0, 1},
+	{1, 1, 0, 1, 1}
+}
+
+
+local roomWidth = #roomLayout[1]
+local roomHeight = #roomLayout
+local roomHeightPx = tileWidth * roomWidth
+local roomWidthPx = tileHeight * roomHeight
+
+local mapWidth = 5 * roomWidth + 1
+local mapHeight = 3 * roomHeight + 1
+
+local mapWidthInPx = roomWidthPx * mapWidth
+local mapHeightInPx = roomHeightPx * mapHeight
+
+local floorCol = {
+	r = 0,
+	b = 26,
+	g = 77
+}
+
+local wallCol = {
+	r = 255,
+	b = 255,
+	g = 55
+}
+
 -- key press callback
 function love.keypressed(key)
 	print(key, string.byte(key), keyMap[key])
@@ -24,7 +75,6 @@ function love.keyreleased(key)
     keys[key] = {down = false} 
 end
 
-
 -- just check if a key is down
 function getKeyDown(key)
 	if not keys[key] then 
@@ -34,7 +84,6 @@ function getKeyDown(key)
 	end
 	return false
 end
-
 
 -- checking if a key is pressed. key will be set as released once checked
 function getKeyPress(key)
@@ -60,6 +109,39 @@ end
 
 function loadGame()
 	newKeyboard()
+
+
+
+
+	for yy=1,mapHeight do
+		for xx=1,mapWidth do
+
+			local rr 
+			local gg
+			local bb 
+			if math.fmod(yy - 1, roomHeight) == 0 or yy == mapHeight or 
+			math.fmod(xx - 1, roomWidth) == 0 or xx == mapWidth then 
+				rr = wallCol.r 
+				gg = wallCol.g 
+				bb = wallCol.b 
+			else 
+				rr = floorCol.r 
+				gg = floorCol.g 
+				bb = floorCol.b 
+			end 
+
+			map[mapWidth * yy + xx] = {
+				r = rr,
+				g = gg,
+				b = bb,
+				
+
+				x = xx, 
+				y = yy
+			}
+		end
+	end
+
 end
 
 
@@ -67,6 +149,21 @@ function updateGame(dt)
 	if getKeyDown("tab") then 
 		newKeyboard()
 	end 
+
+	if getKeyDown("up") then 
+		camera.y = camera.y - 10
+	elseif getKeyDown("down") then 
+		camera.y = camera.y + 10
+	end 
+
+	if getKeyDown("left") then 
+		camera.x = camera.x - 10
+	elseif getKeyDown("right") then 
+		camera.x = camera.x + 10
+	end 
+
+	if getKeyDown("q") then camera.scale = camera.scale - dt end 
+	if getKeyDown("e") then camera.scale = camera.scale + dt end 
 end
 
 function colourSet(character)
@@ -78,9 +175,9 @@ function colourSet(character)
 end 
 
 
-local keyboardTopLeft = { x = 100, y = 100 }
 
-function drawKey(character, x, y)	
+
+function drawKey(character, x, y, keyboardTopLeft)	
 	love.graphics.setColor(0, 0, 200)
 	love.graphics.rectangle("fill", keyboardTopLeft.x + (20 * x) + (5*y), keyboardTopLeft.y + (20 * y), 20, 20)
 	love.graphics.setColor(0, 0, 150)
@@ -89,26 +186,59 @@ function drawKey(character, x, y)
 	love.graphics.print(keyMap[character], keyboardTopLeft.x + (20 * x) + (5*y) + 5, keyboardTopLeft.y + (20 * y) + 5)	
 end 
 
-function drawGame()
+function drawKeyboard(_x, _y)
+	local keyboardTopLeft = { x = _x, y = _y }
 
 	local counter = 0
 	for c in topRow:gmatch"." do
-		drawKey(c, counter, 0)
+		drawKey(c, counter, 0, keyboardTopLeft)
 		counter = counter + 1
 	end
 
 	counter = 0
 	for c in secondRow:gmatch"." do
-		drawKey(c, counter, 1)
+		drawKey(c, counter, 1, keyboardTopLeft)
 		counter = counter + 1
 	end
 	
 	counter = 0
 	for c in thirdRow:gmatch"." do
-		drawKey(c,  counter, 2)
+		drawKey(c,  counter, 2, keyboardTopLeft)
 		counter = counter + 1
 	end
+end 
 
+function drawGame()
+
+	drawKeyboard(100, 100)
+
+	love.graphics.origin()
+	love.graphics.push()
+
+		-- camera update
+		love.graphics.scale(camera.scale)		
+		--love.graphics.translate((screenWidth / 2), (screenHeight / 2))
+			love.graphics.translate((-camera.x), (-camera.y))	
+			--love.graphics.rotate(camera.rotation)
+			--love.graphics.scale(camera.scale)	
+		--love.graphics.translate((-screenWidth / 2), (-screenHeight / 2))
+	
+
+			
+
+
+		for y=1,mapHeight do
+			for x=1,mapWidth do 
+				if x * tileWidth >= camera.x and x * tileWidth < camera.x + roomWidthPx then 
+					love.graphics.setColor(map[mapWidth*y+x].r, map[mapWidth*y+x].g, map[mapWidth*y+x].b)
+					love.graphics.rectangle("fill", (map[mapWidth*y+x].x-1)*tileWidth, 
+													(map[mapWidth*y+x].y-1)*tileHeight, 
+													tileWidth, tileHeight)
+
+				end 
+			end 
+		end
+	love.graphics.pop()
 end
 
 
